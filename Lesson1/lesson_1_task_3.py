4c0a7a0c63c07bb3f36362b29f1374a727ae4df7
ElliotSalem9601 <116618192+ElliotSalem9601@users.noreply.github.com>
2026-04-08 14:20:29 +1100

    Update lesson_1_task_3.py

--- Lesson1/lesson_1_task_3.py
+++ Lesson1/lesson_1_task_3.py
@@ -1,4 +1,6 @@
-first_name = "Эдуард"
-last_name = "Пак"  
+# Получение пользовательского ввода
+
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
 
 print(f"Вас зовут: {last_name} {first_name}")
