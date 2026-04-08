95fc5d453233425a9eca36acfb94974bf789bbd3
ElliotSalem9601 <116618192+ElliotSalem9601@users.noreply.github.com>
2026-04-08 14:21:40 +1100

    Update lesson_1_task_5.py

--- Lesson1/lesson_1_task_5.py
+++ Lesson1/lesson_1_task_5.py
@@ -1,18 +1,8 @@
 # Параметризация функции
 
-def print_num(num):
-    print(num)
+def print_phone_number():
+    print("88005553535")
 
 
-# Вызов функции 11 раз для вывода номера 88005553535
-print_num(8)
-print_num(8)
-print_num(0)
-print_num(0)
-print_num(5)
-print_num(5)
-print_num(5)
-print_num(3)
-print_num(5)
-print_num(3)
-print_num(5)
+# Вызов функции один раз
+print_phone_number()
