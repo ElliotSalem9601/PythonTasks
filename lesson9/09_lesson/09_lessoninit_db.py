2684cf79617fa4fe5e87b6a16cfa892d47c9b8ce
ElliotSalem9601 <116618192+ElliotSalem9601@users.noreply.github.com>
2026-04-16 14:22:33 +1100

    Delete lesson9/09_lesson/09_lessoninit_db.py

--- lesson9/09_lesson/09_lessoninit_db.py
+++
@@ -1,9 +0,0 @@
-"""Скрипт для инициализации базы данных"""
-
-from db_config import init_db, engine
-from models import Base
-
-if __name__ == "__main__":
-    print("Создание таблиц в базе данных...")
-    Base.metadata.create_all(engine)
-    print("Таблицы успешно созданы!")
